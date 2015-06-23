eb74bebf11bd086a94da1e64e8b6a987f05c109e
Pascal Monasse <monasse@imagine.enpc.fr>
2015-06-23 17:01:59 +0200

    Change code for compatibility with IPOL article

--- src/libOrsa/fundamental_model.cpp
+++ src/libOrsa/fundamental_model.cpp
@@ -98,39 +98,42 @@ void FundamentalModel::Fit(const std::vector<int> &indices,
   EncodeEpipolarEquation(x1_, x2_, indices, &A);
 
   if(indices.size() >= 8) { // 8-point algorithm
-    // Without constraint
+    // Without rank constraint
     libNumerics::vector<double> vecNullspace(9);
     libNumerics::SVD::Nullspace(A, &vecNullspace, 1, 1);
     libNumerics::matrix<double> F(3,3);
     F.read(vecNullspace);
 
-    // Force the fundamental property if the A matrix has full rank.
+    // Force the rank 2 constraint
     libNumerics::matrix<double> FRank2(3,3);
     libNumerics::SVD::EnforceRank2_3x3(F, &FRank2);
     Fs->push_back(FRank2);
-  }
-  else
-  {
-    // Find the two F matrices in the nullspace of A.
+  } else { // 7-point algorithm
+    // Find the two F matrices in the nullspace of A
     Mat F1(3,3), F2(3,3);
     libNumerics::SVD::Nullspace2_Remap33(A,F1,F2);
+    F2 -= F1;
 
-    // Then, use the condition det(F) = 0 to determine F. In other words, solve
-    // det(F1 + t*F2) = 0 for t.
-    Vec v1=F1.col(0), v2=F1.col(1), v3=F1.col(2);
-    Vec w1=F2.col(0), w2=F2.col(1), w3=F2.col(2);
+    // Use condition det(F)=0 to determine F: solve det(F1 + t*F2) = 0 for t
+    Vec c1=F1.col(0), c2=F1.col(1), c3=F1.col(2);
+    Vec d1=F2.col(0), d2=F2.col(1), d3=F2.col(2);
     double P[4] = {
-        det3(v1,v2,v3),
-        det3(w1,v2,v3)+det3(v1,w2,v3)+det3(v1,v2,w3),
-        det3(v1,w2,w3)+det3(w1,v2,w3)+det3(w1,w2,v3),
-        det3(w1,w2,w3)
+        det3(c1,c2,c3),
+        det3(d1,c2,c3)+det3(c1,d2,c3)+det3(c1,c2,d3),
+        det3(c1,d2,d3)+det3(d1,c2,d3)+det3(d1,d2,c3),
+        det3(d1,d2,d3)
     };
+    if(std::abs(P[0]) > std::abs(P[3])) {
+        libNumerics::swap(F1,F2);
+        std::swap(P[0],P[3]);
+        std::swap(P[1],P[2]);
+    }
 
-    // Solve for the roots of P[3]*x^3 + P[2]*x^2 + P[1]*x + P[0] = 0.
+    // Find roots of polynomial P[3]*t^3 + P[2]*t^2 + P[1]*t + P[0]
     double roots[3];
     int num_roots = CubicRoots(P, roots);
 
-    // Build the fundamental matrix for each solution.
+    // Build fundamental matrix for each solution
     for (int s = 0; s < num_roots; ++s)
       Fs->push_back(F1 + roots[s] * F2);
   }
